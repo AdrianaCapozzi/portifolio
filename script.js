28c5b2e37acf8337bb8c20bdc7375914372d5edf
Adriana Capozzi <145504525+AdrianaCapozzi@users.noreply.github.com>
2025-08-17 11:27:50 -0300

    add pic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function aumentarFoto() {
-    const foto = document.getElementById("minha-foto");
-    foto.style.transform = foto.style.transform === "scale(1.5)" ? "scale(1)" : "scale(1.5)";
+  const foto = document.getElementById('minha-foto');
+  foto.style.transform = foto.style.transform === 'scale(1.1)' ? 'scale(1)' : 'scale(1.1)';
 }
