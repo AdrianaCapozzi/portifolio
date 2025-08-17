63d79462bb7160445a567c12842613cfe1cec09f
Adriana Capozzi <145504525+AdrianaCapozzi@users.noreply.github.com>
2025-08-17 12:56:39 -0300

    update pic

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function aumentarFoto() {
   const foto = document.getElementById('minha-foto');
-  foto.style.transform = foto.style.transform === 'scale(1.1)' ? 'scale(1)' : 'scale(1.1)';
+  foto.style.transform =
+    foto.style.transform === 'scale(1.1)' ? 'scale(1)' : 'scale(1.1)';
 }
+
+document.getElementById('minha-foto').addEventListener('click', aumentarFoto);
